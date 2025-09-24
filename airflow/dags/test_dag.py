356c04d9f424713948fe6d566bcdbfb9715ec2c0
Pham Hieu <hieuphamsaga@gmail.com>
2025-09-24 12:06:33 +0700

    add spark

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -9,26 +9,24 @@ default_args = {
 }
 
 with DAG(
-    "spark_trino_etl",
+    "spark_csv_parquet_etl",
     default_args=default_args,
-    description="Run Spark job on MinIO data and validate with Trino",
-    schedule_interval=None,   
+    schedule_interval=None,
     start_date=datetime(2025, 1, 1),
     catchup=False,
-    tags=["lakehouse", "spark", "trino"],
 ) as dag:
 
-    spark_csv_job = SparkSubmitOperator(
-        task_id="spark_csv_job",
-        application="/opt/spark/jobs/test_csv_to_iceberg.py", 
-        jars="/opt/extra-jars/hadoop-aws-3.3.2.jar,/opt/extra-jars/aws-java-sdk-bundle-1.11.901.jar,/opt/extra-jars/iceberg-spark-runtime-3.5_2.12-1.10.0.jar",
+    spark_csv_to_parquet = SparkSubmitOperator(
+        task_id="spark_csv_to_parquet",
+        application="/opt/spark/jobs/test_csv_in_minio.py",  
+        conn_id="spark_default",  
+        jars="/opt/extra-jars/hadoop-aws-3.3.2.jar,/opt/extra-jars/aws-java-sdk-bundle-1.11.901.jar",
         conf={
-        "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
-        "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
-        "spark.hadoop.fs.s3a.access.key": "admin",
-        "spark.hadoop.fs.s3a.secret.key": "password",
-        "spark.hadoop.fs.s3a.path.style.access": "true"
+            "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+            "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
+            "spark.hadoop.fs.s3a.access.key": "admin",
+            "spark.hadoop.fs.s3a.secret.key": "password",
+            "spark.hadoop.fs.s3a.path.style.access": "true",
         },
-        conn_id="spark-air", 
         verbose=True,
     )
